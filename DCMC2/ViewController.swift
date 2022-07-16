d0ac80db0fdf5962339fe489b3bf3593c6223017
thienlm <thienlm@imt-soft.com>
2022-07-16 18:42:53 +0700

    Initial Commit

---
+++ DCMC2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DCMC2
+//
+//  Created by Cuong Nguyen on 16/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
